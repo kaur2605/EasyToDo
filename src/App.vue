5690edc1f8ef7e44e07d67e7d1d61eb337c58b73
Gurdeep Kaur <kaur12@outlook.com>
2021-05-08 19:54:26 +0200

    added funtionalities and design fix

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div id="app">
     <EasyToDo></EasyToDo>
   </div>
 </template>
--- src/components/EasyToDo.vue
+++ src/components/EasyToDo.vue
@@ -1,49 +1,72 @@
 <template>
   <div class="mt-10 w-96 m-auto">
-    <div class="my-10 flex justify-center text-2xl">
-      <h1 class="font-bold text-green-600 text-center">Easy</h1>
-      <h1 class="font-bold text-grey-700 text-center">ToDoApp</h1>
+    <div class="my-10 flex justify-center text-center text-2xl">
+      <h1 class="font-bold text-green-600 text-center font-sans">Easy</h1>
+      <h1 class="font-bold text-grey-700 text-center font-sans">ToDoApp</h1>
     </div>
     <div class="">
       <input
         type="text"
-        class="w-48 h-8 border-2"
-        placeholder="What needs to be done"
+        class="w-48 h-10 border-2 border-transparent rounded-sm"
+        placeholder="Enter somthing to do..."
         v-model="newTodo"
       />
       <button
-        class="bg-green-600 border-2 rounded-sm px-2 w-26 h-8 text-sm"
+        class="bg-green-600 border-2 rounded-r-lg px-2 w-26 h-10 text-sm"
         @click="addTodo"
       >
         Create To-do
       </button>
     </div>
     <div class="w-72">
-      <h5 class="text-left h-4 mt-2 c font-normal text-sm leading-4">
+      <h5
+        class="normal text-left h-4 mt-2 c font-normal text-sm leading-4 font-sans"
+      >
         All To-Do's
       </h5>
-      <ul
-        v-for="(todo, index) in todos"
-        :key="todo.id"
-        class="flex justify-between border-2 rounded-lg w-72 m-auto mt-6"
-      >
-        <li :class="{ todo__item__toggle: todo.completed }">
-          {{ todo.title }}
-          <div class="flex justify-between">
-            <div>
-              <button
-                class="mt-1 w-4 h-4 ml-2 pl-2"
-                type="checkbox"
-                @click="completeTask(todo)"
-              >
-                <div class="">
-                  <font-awesome-icon :icon="['fas', 'check']" />
-                </div>
-              </button>
+      <ul v-for="(todo, index) in todos" :key="todo.id">
+        <li
+          :class="{ checkIcon: todo.completed }"
+          class="flex justify-between border-2 rounded-lg w-72 m-auto mt-6 border-transparent"
+        >
+          <div
+            v-if="todo.showTemp"
+            class="text-center w-18 h-12 bg-green-500"
+            @click="completeTask(todo)"
+          >
+            <div class="p-4">
+              <font-awesome-icon
+                :icon="['fas', 'check']"
+                class="checkIcon text-white"
+              />
             </div>
-            <div class="ml-3 w-8 h-8 bg-red-500" @click="removeTodo(index)">
-              <div class="p-2 pb-4">
-                <font-awesome-icon :icon="['fas', 'trash-alt']" />
+          </div>
+          <div class="normal pl-2 pt-2 font-sans">
+            {{ todo.title }}
+          </div>
+
+          <div class="flex">
+            <div
+              v-if="todo.showIcons"
+              class="text-center w-18 h-12 bg-green-500"
+              @click="completeTask(todo)"
+            >
+              <div class="p-4">
+                <font-awesome-icon
+                  :icon="['fas', 'check']"
+                  class="checkIcon text-white"
+                />
+              </div>
+            </div>
+            <div
+              class="text-center w-18 h-12 bg-red-500"
+              @click="removeTodo(index)"
+            >
+              <div class="p-4">
+                <font-awesome-icon
+                  :icon="['fas', 'trash-alt']"
+                  class="text-white"
+                />
               </div>
             </div>
           </div>
@@ -62,19 +85,22 @@ export default {
       idForTodo: 3,
       completed: false,
       beforeEditCache: '',
-      filter: 'all',
+
+
       todos: [
         {
           'id': 1,
           'title': 'Finish Vue Screencast',
           'completed': false,
-          'editing': false,
+          'showTemp': false,
+          'showIcons': true,
         },
         {
           'id': 2,
           'title': 'Take over world',
           'completed': false,
-          'editing': false,
+          'showTemp': false,
+          'showIcons': true,
         },
       ]
     }
@@ -95,7 +121,7 @@ export default {
         id: this.idForTodo,
         title: this.newTodo,
         completed: false,
-        editing: false,
+        showIcons: true,
       })
       this.newTodo = ''
       this.idForTodo++
@@ -104,12 +130,11 @@ export default {
     removeTodo(index) {
       this.todos.splice(index, 1)
     },
-    isCompleted(todo) {
-      return todo.completed;
-    },
     completeTask(todo) {
       console.log(todo);
       todo.completed = !todo.completed
+      todo.showIcons = false;
+      todo.showTemp = true;
 
     }
   }
@@ -119,19 +144,9 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 120px;
-}
-
-.todo__item__toggle {
-  cursor: pointer;
-  width: 100%;
-  text-align: left;
-  background-color: lightgreen;
+.checkIcon {
+  background-color: #10b981;
+  color: white;
+  margin-right: 100%;
 }
 </style>
